be88b13a38d1a22576f50046dae36fb05e3bebf2
SergioScata <sergiopio395@gmail.com>
2024-01-09 13:25:23 +0100

    Commit

--- src/main/java/org/learning/springpizzeria/controller/PizzaController.java
+++ src/main/java/org/learning/springpizzeria/controller/PizzaController.java
@@ -3,12 +3,16 @@ package org.learning.springpizzeria.controller;
 import org.learning.springpizzeria.model.Pizza;
 import org.learning.springpizzeria.repository.PizzaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/pizza")
@@ -21,6 +25,18 @@ public class PizzaController {
         List<Pizza> pizzaList = pizzaRepository.findAll();
         model.addAttribute("pizzaList", pizzaList);
         return "pizzas/list";
+    }
+
+    @GetMapping("/show/{id}")
+    public String show(@PathVariable Integer id, Model model) {
+        Optional<Pizza> result = pizzaRepository.findById(id);
+        if (result.isPresent()) {
+            Pizza pizza = result.get();
+            model.addAttribute("pizza", pizza);
+            return "pizzas/show";
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Pizza with id " + id + " not found");
+        }
     }
 
 }
